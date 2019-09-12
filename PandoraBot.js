08799b778639f36d912550756b6502c8c790328a
codemike@naver.com <codemike@naver.com>
2019-09-12 16:42:54 +0900

    feat - 진영 식품 장부 확인 기능 추가

--- PandoraBot.js
+++ PandoraBot.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
@@ -14,14 +15,10 @@ app.use('/api', apiRouter);
 
 const fs = require('fs');
 
-
 const readline = require('readline');
 const {google} = require('googleapis');
 
 var _auth;
-var testpara = {
-  name : "문주한"
-}
 
 var obj = {
    table: []
@@ -33,15 +30,13 @@ fs.readFile("PandoraBotJSON.json", function(err, data) {
 
 // 최초 이름 체크
 apiRouter.post('/nameInput', function(req, res) {
-  var returnText = _nameCheck(req.body.userRequest.user.id, req.body.userRequest.utterance);
-
   const responseBody = {
     version: "2.0",
     template: {
       outputs: [
         {
           simpleText: {
-            text: returnText
+            text: _nameCheck(req.body.userRequest.user.id, req.body.userRequest.utterance)
           }
         }
       ],
@@ -57,20 +52,19 @@ apiRouter.post('/nameInput', function(req, res) {
     ]
     }
   };
-  console.log("nameInput" + req.body.userRequest.utterance);
+  console.log("nameInput " + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
 // 이름 확인
 apiRouter.post('/nameCheck', function(req, res) {
-  var returnText = _nameCheck(req.body.userRequest.user.id, "NULL");
   const responseBody = {
     version: "2.0",
     template: {
       outputs: [
         {
           simpleText: {
-            text: returnText
+            text: _nameCheck(req.body.userRequest.user.id, "NULL")
           }
         }
       ],
@@ -94,68 +88,51 @@ apiRouter.post('/nameCheck', function(req, res) {
     ]
     }
   };
-  _displayAccount(req.body.userRequest.user.id);
-  console.log("nameCheck" + req.body.userRequest.utterance);
+  console.log("nameCheck " + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
 // JSON내 이름 체크 함수
-function _nameCheck(userID, userName) {
-  var check = false;
-  var name;
-  var num;
-  for(var item in obj.table){
-      if(obj.table[item].id == userID){
-        console.log("SAME");
-        name = obj.table[item].name;
-        check = true;
-        num = item;
-      }
-  }
+function _nameCheck(_userID, userName) {
+  var userNum = _checkJSON(_userID);
 
-  if(userName == "D"){
-    console.log("NUM" + num);
-    obj.table.splice(num, 1);
-    var json = JSON.stringify(obj);
-    fs.writeFile('PandoraBotJSON.json', json);
-    check = false;
-
-    return "처음 접속 하였습니다. 이름을 정확히 입력해주세요.";
-  }
-
-  if(check){
-    return name + "님 안녕하세요.";
-  }else{
+  if(userNum == "empty"){
+    // JSON에 이름 없음
     if(userName == "NULL"){
       return "처음 접속 하였습니다. 이름을 정확히 입력해주세요.";
     }else{
-      obj.table.push({id:userID, name:userName});
+      obj.table.push({id:_userID, name:userName});
       var json = JSON.stringify(obj);
       fs.writeFile('PandoraBotJSON.json', json);
       return "처음 접속 하였습니다." + userName + "님 안녕하세요.";
     }
+  }else{
+    // JSON에 이름 있음
+    if(userName == "D"){
+      // 이름 수정
+      obj.table.splice(userNum, 1);
+      var json = JSON.stringify(obj);
+      fs.writeFile('PandoraBotJSON.json', json);
+      return "처음 접속 하였습니다. 이름을 정확히 입력해주세요.";
+    }
+    return obj.table[userNum].name + "님 안녕하세요.";
   }
 }
 
 // 이름 수정
 apiRouter.post('/editName', function(req, res) {
-  var returnText = _nameCheck(req.body.userRequest.user.id, "D");
-
   const responseBody = {
     version: "2.0",
     template: {
       outputs: [
         {
           simpleText: {
-            text: returnText
+            text: _nameCheck(req.body.userRequest.user.id, "D")
           }
         }
-      ]
-    }
-  };
+      ]}};
 
-  console.log("editName" + req.body.userRequest.utterance);
-  console.log("P"+app.purpose);
+  console.log("editName " + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
@@ -183,41 +160,22 @@ apiRouter.post('/inputPurpose', function(req, res) {
     }
   };
   _writePurpose(req.body.userRequest.user.id, req.body.userRequest.utterance);
-  // req.body.userRequest.utterance;
-  console.log("inputPurpose" + req.body.userRequest.utterance);
+  console.log("inputPurpose" + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
 // JSON내 목적 쓰기
-function _writePurpose(userID, purpose) {
-  var check = false;
-  for(var item in obj.table){
-      if(obj.table[item].id == userID){
-        check = true;
-
-        obj.table[item].type = "PA";
-        obj.table[item].purpose = purpose;
-        var json = JSON.stringify(obj);
-        fs.writeFile('PandoraBotJSON.json', json);
-
-      }
-  }
-
-  if(check){
-    return "금액을 정확히 입력해 주십시오.";
-  }else{
+function _writePurpose(_userID, purpose) {
+  var userNum = _checkJSON(_userID);
+  if(userNum == "empty"){
     return "오류가 발생했습니다. 처음부터 다시 해주십시오.";
   }
-}
+  obj.table[userNum].type = "PA";
+  obj.table[userNum].purpose = purpose;
 
-// 본인 JSON 내용 콘솔 출력
-function _displayAccount(userID){
-  for(var item in obj.table){
-    if(obj.table[item].id == userID){
-      console.log(obj.table[item]);
-      }
-    }
-  }
+  writeJSON();
+  return "금액을 정확히 입력해 주십시오.";
+}
 
 // 금액 입력
 apiRouter.post('/inputMoney', function(req, res) {
@@ -226,40 +184,16 @@ apiRouter.post('/inputMoney', function(req, res) {
     template: _writeMoney(req.body.userRequest.user.id, req.body.userRequest.utterance, req._startTime)
 
   };
-  console.log("inputMoney " + req.body.userRequest.utterance);
+  console.log("inputMoney " + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
 // JSON내 금액 쓰기
-function _writeMoney(userID, money, time) {
-  var check = false;
-  for(var item in obj.table){
-      if(obj.table[item].id == userID){
-        check = true;
-
-        obj.table[item].type = "PA";
-        obj.table[item].money = money;
-        obj.table[item].time = time;
-
-        var returnText = obj.table[item].name + " " + obj.table[item].purpose + " " + obj.table[item].money + "\n전송 하려면 전송코드를 입력하시오.";
-        var json = JSON.stringify(obj);
-        fs.writeFile('PandoraBotJSON.json', json);
-      }
-  }
-
+function _writeMoney(_userID, money, time) {
+  var userNum = _checkJSON(_userID);
   var returnObj;
-  if(check){
-    returnObj = {template: {
-      outputs: [
-        {
-          simpleText: {
-            text: returnText
-          }
-        }
-      ]
-  }};
-    return returnObj.template;
-  }else{
+
+  if(userNum == "empty"){
     returnObj = {template: {
       outputs: [
         {
@@ -281,16 +215,33 @@ function _writeMoney(userID, money, time) {
   }};
     return returnObj.outputs;
   }
+  obj.table[userNum].type = "PA";
+  obj.table[userNum].money = money;
+  obj.table[userNum].time = time;
+  var returnText = obj.table[userNum].name + " " + obj.table[userNum].purpose + " " + obj.table[userNum].money + "\n전송 하려면 전송코드를 입력하시오.";
+
+  writeJSON();
+
+  returnObj = {template: {
+    outputs: [
+      {
+        simpleText: {
+          text: returnText
+        }
+      }
+    ]
+  }};
+  return returnObj.template;
 }
 
 // 본인 JSON 입력값 반환
-function _inputCheck(userID){
-
+function _checkJSON(_userID){
   for(var item in obj.table){
-    if(obj.table[item].id == userID){
-      return obj.table[item];
+    if(obj.table[item].id == _userID){
+      return item;
     }
   }
+  return "empty";
 }
 
 // ------- 진영 식품 --------
@@ -309,39 +260,110 @@ apiRouter.post('/JYF_inputMenu', function(req, res) {
     }
   };
 
-  console.log("JYF_inputMenu" + req.body.userRequest.utterance);
+  console.log("JYF_inputMenu" + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
+  res.status(200).send(responseBody);
+});
+
+// JSON내 진영식품 메뉴 쓰기
+function _JYFwriteMenu(_userID, menu, time) {
+  var returnText;
+  var userNum = _checkJSON(_userID);
+
+  if(userNum == "empty"){
+    return "오류가 발생했습니다. 처음부터 다시 해주십시오.";
+  }
+
+  obj.table[userNum].type = "JYF";
+  obj.table[userNum].jyf_menu = menu;
+  obj.table[userNum].time = time;
+
+  returnText = obj.table[userNum].name + " " + obj.table[userNum].jyf_menu + "\n전송 하려면 전송코드를 입력하시오.";
+  writeJSON();
+
+  return returnText;
+}
+
+// 본인 장부 확인
+apiRouter.post('/JYF_checkBill', function(req, res) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: _JYFloadBill(req.body.userRequest.user.id)
+          }
+        }
+      ],
+      quickReplies: [
+      {
+        action: "block",
+        label: "처음으로",
+        messageText: "처음으로",
+        blockId: "5ceb722905aaa7533585ab8b",
+        extra: {
+        }
+      }
+    ]
+    }
+  };
+
+  console.log("JYF_checkBill" + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
 
   console.log(responseBody);
   res.status(200).send(responseBody);
 });
 
-// JSON내 진영식품 메뉴 쓰기
-function _JYFwriteMenu(userID, menu, time) {
-  var check = false;
+// JSON에서 장부 읽기
+function _JYFloadBill(_userID) {
   var returnText;
-  for(var item in obj.table){
-      if(obj.table[item].id == userID){
-        check = true;
-
-        obj.table[item].type = "JYF";
-        obj.table[item].jyf_menu = menu;
-        obj.table[item].time = time;
-
-        returnText = obj.table[item].name + " " + obj.table[item].jyf_menu + "\n전송 하려면 전송코드를 입력하시오.";
-        var json = JSON.stringify(obj);
-        fs.writeFile('PandoraBotJSON.json', json);
-      }
-  }
-console.log(check);
-console.log(returnText);
-
-  if(check){
-    return returnText;
-  }else{
+  var userNum = _checkJSON(_userID);
+  exportJson(userNum);
+  if(userNum == "empty"){
     return "오류가 발생했습니다. 처음부터 다시 해주십시오.";
   }
+  // !!!!!!!!!!!!!!!!!!!수정해야댐
+  returnText = "-- " + obj.table[userNum].name + "님의 장부 --\n참깨라면 " + obj.table[userNum].jyf_num1 + "개\n박카스 " + obj.table[userNum].jyf_num2 + "개\n총액 " + obj.table[userNum].jyf_total + "";
+  // !!!!!!!!!!!!!!!!!!!!!!!!!
+  return returnText;
 }
+
+// 시트JSON에서 데이터 추출해서 JSON에 쓰기
+function exportJson(_userNum) {
+  var sheetDataLink_JYF = "https://spreadsheets.google.com/feeds/cells/1llk5IZ41U5Ul3kOQva8jkZwZlreHBmtzTwhgTwpeXGo/2/public/basic?alt=json-in-script&min-col=6&max-col=9&min-row=5";
+  var sheetJson;
+
+  axios.get(sheetDataLink_JYF).then(function(response) {
+    sheetJson = response.data.slice(28,response.data.length-2);
+    entry = JSON.parse(sheetJson).feed.entry;
+
+    for(var i in entry){
+      if(entry[i].content.$t == obj.table[_userNum].name){
+        var num = i;
+        num++;
+        obj.table[_userNum].jyf_num1 = entry[num].content.$t;
+        num++;
+        obj.table[_userNum].jyf_num2 = entry[num].content.$t;
+        num++;
+        obj.table[_userNum].jyf_total = entry[num].content.$t;
+        writeJSON();
+        return 0;
+      }
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
+
 // ^------ 진영 식품 -------^
+
+// JSON 저장
+function writeJSON(){
+  var json = JSON.stringify(obj);
+  fs.writeFile('PandoraBotJSON.json', json);
+
+}
 
 // 데이터 전송
 apiRouter.post('/sendData', function(req, res) {
@@ -357,11 +379,10 @@ apiRouter.post('/sendData', function(req, res) {
       ]
     }
   };
-  var sendObj = _inputCheck(req.body.userRequest.user.id);
+  var sendObj = obj.table[_checkJSON(req.body.userRequest.user.id)];
 
   callAppsScript(_auth, sendObj);
-  console.log("sendData " + req.body.userRequest.utterance);
-  console.log(responseBody);
+  console.log("sendData " + obj.table[_checkJSON(req.body.userRequest.user.id)].name + req.body.userRequest.utterance);
   res.status(200).send(responseBody);
 });
 
