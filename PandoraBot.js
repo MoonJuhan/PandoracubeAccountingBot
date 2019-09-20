521651eb94f134a44c464799f23a0ac44f13feb6
codemike@naver.com <codemike@naver.com>
2019-09-20 17:45:40 +0900

    feat - 진영 식품 순위 보여주기 기능 추가 fixed #4

--- PandoraBot.js
+++ PandoraBot.js
@@ -356,14 +356,13 @@ function _JYFloadBill(_userID) {
   if(userNum == "empty"){
     return "오류가 발생했습니다. 처음부터 다시 해주십시오.";
   }
-
-  returnText = "-- " + obj.table[userNum].name + "님의 장부 --\n참깨라면 " + obj.table[userNum].jyf_num1 + "개\n박카스 " + obj.table[userNum].jyf_num2 + "개\n총액 " + obj.table[userNum].jyf_total + "";
+  returnText = "-- " + obj.table[userNum].name + "님의 장부 --\n등수 : " + obj.table[userNum].jyf_ranking +"\n참깨라면 " + obj.table[userNum].jyf_num1 + "개\n박카스 " + obj.table[userNum].jyf_num2 + "개\n총액 " + obj.table[userNum].jyf_total + "";
   return returnText;
 }
 
 // 시트JSON에서 데이터 추출해서 JSON에 쓰기
 function exportJson(_userNum) {
-  var sheetDataLink_JYF = "https://spreadsheets.google.com/feeds/cells/1llk5IZ41U5Ul3kOQva8jkZwZlreHBmtzTwhgTwpeXGo/2/public/basic?alt=json-in-script&min-col=6&max-col=9&min-row=5";
+  var sheetDataLink_JYF = "https://spreadsheets.google.com/feeds/cells/1llk5IZ41U5Ul3kOQva8jkZwZlreHBmtzTwhgTwpeXGo/1/public/basic?alt=json-in-script&min-col=2&max-col=6&min-row=5";
 
   axios.get(sheetDataLink_JYF).then(function(response) {
     var sheetJson = response.data.slice(28,response.data.length-2);
@@ -378,11 +377,17 @@ function exportJson(_userNum) {
         obj.table[_userNum].jyf_num2 = entry[num].content.$t;
         num++;
         obj.table[_userNum].jyf_total = entry[num].content.$t;
-        console.dir(obj.table[_userNum]);
+        num++;
+        obj.table[_userNum].jyf_ranking = entry[num].content.$t;
         writeJSON();
         return 0;
       }
     }
+    obj.table[_userNum].jyf_num1 = 0;
+    obj.table[_userNum].jyf_num2 = 0;
+    obj.table[_userNum].jyf_total = 0;
+    obj.table[_userNum].jyf_ranking = "X";
+
   })
   .catch(function(error) {
     console.log(error);
