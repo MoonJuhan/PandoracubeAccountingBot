25f2a1fa457d201d1d289715cbc229c72c173a43
codemike@naver.com <codemike@naver.com>
2019-09-24 16:41:36 +0900

    fix - 요일 입력 오류 수정

--- JYF_DriectScript.js
+++ JYF_DriectScript.js
@@ -55,25 +55,25 @@ function writeDateSell(_writePos, _num1, _num2, _date){
   var day;
   switch(time.getDay()){
     case 0:
-      day = "월요일"
-      break;
-    case 1:
-      day = "화요일"
-      break;
-    case 2:
-      day = "수요일"
-      break;
-    case 3:
-      day = "목요일"
-      break;
-    case 4:
-      day = "금요일"
-      break;
-    case 5:
       day = "토요일"
       break;
-    case 6:
+    case 1:
       day = "일요일"
+      break;
+    case 2:
+      day = "월요일"
+      break;
+    case 3:
+      day = "화요일"
+      break;
+    case 4:
+      day = "수요일"
+      break;
+    case 5:
+      day = "목요일"
+      break;
+    case 6:
+      day = "금요일"
       break;
   }
   sheet_Analysis.getRange("I"+_writePos).setValue(day);
