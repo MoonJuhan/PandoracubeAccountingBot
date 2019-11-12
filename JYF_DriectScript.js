96eb21f908f60bc1f90f6b5bdfb97a1730fb99ec
codemike@naver.com <codemike@naver.com>
2019-11-12 10:34:37 +0900

    fix - 진영 식품 스크립트 절대 주소 수정

--- JYF_DriectScript.js
+++ JYF_DriectScript.js
@@ -5,18 +5,18 @@ var sheet_Hide = GSS.getSheetByName("숨김시트");
 
 function myFunction() {
   var date = dayUpdate();
-  sheet_Hide.getRange("J3").setValue(date);
+  sheet_Hide.getRange("P3").setValue(date);
 
   var check = true;
   var menu1Num = 0;
   var menu2Num = 0;
   var menu3Num = 0;
-  var scanPos = sheet_Hide.getRange("J5").getValue();
+  var scanPos = sheet_Hide.getRange("P5").getValue();
 
   if(scanPos != "#N/A"){
     while(check){
       var datePos = "B"+scanPos;
-      if(sheet_Hide.getRange("J4").getValue().toString().substring(8, 10) != sheet_AutoWrite.getRange(datePos).getValue().toString().substring(8, 10)){
+      if(sheet_Hide.getRange("P4").getValue().toString().substring(8, 10) != sheet_AutoWrite.getRange(datePos).getValue().toString().substring(8, 10)){
         check = false;
         break;
       }
@@ -35,15 +35,16 @@ function myFunction() {
       scanPos++;
     }
 
-    var writePos = sheet_Hide.getRange("J6").getValue();
+    var writePos = sheet_Hide.getRange("P6").getValue();
 
-    writeDateSell(writePos, menu1Num, menu2Num, menu3Num, sheet_Hide.getRange("J4").getValue());
+    writeDateSell(writePos, menu1Num, menu2Num, menu3Num, sheet_Hide.getRange("P4").getValue());
   }
 
 }
 
 function dayUpdate(){
   var time = new Date();
+  console.log(time);
   var month = time.getMonth()+1;
   var day = time.getDate();
   var returnText = time.getFullYear() + ". " +month + ". " + day;
@@ -52,7 +53,7 @@ function dayUpdate(){
 }
 
 function writeDateSell(_writePos, _num1, _num2, _num3, _date){
-  sheet_Analysis.getRange("I"+_writePos).setValue(_date);
+  sheet_Analysis.getRange("F"+_writePos).setValue(_date);
 
   var time = new Date();
   var day;
@@ -79,8 +80,8 @@ function writeDateSell(_writePos, _num1, _num2, _num3, _date){
       day = "금요일"
       break;
   }
-  sheet_Analysis.getRange("J"+_writePos).setValue(day);
-  sheet_Analysis.getRange("K"+_writePos).setValue(_num1);
-  sheet_Analysis.getRange("L"+_writePos).setValue(_num2);
-  sheet_Analysis.getRange("M"+_writePos).setValue(_num3);
+  sheet_Analysis.getRange("G"+_writePos).setValue(day);
+  sheet_Analysis.getRange("H"+_writePos).setValue(_num1);
+  sheet_Analysis.getRange("I"+_writePos).setValue(_num2);
+  sheet_Analysis.getRange("J"+_writePos).setValue(_num3);
 }
