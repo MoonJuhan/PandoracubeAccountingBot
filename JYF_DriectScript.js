b2589fe9a62a50ff39de95b72efa510c41b477c8
codemike@naver.com <codemike@naver.com>
2019-11-14 13:16:11 +0900

    feat - 진영 식품 직할 스크립트 셀 주소 찾기 수정
    절대 주소에서 상대 주소로 셀의 주소 찾기 변경을 통해 시트 변화에 대응이 쉬워짐

--- JYF_DriectScript.js
+++ JYF_DriectScript.js
@@ -1,27 +1,17 @@
 var GSS = SpreadsheetApp.getActive();
 var sheet_AutoWrite = GSS.getSheetByName("자동입력시트");
 var sheet_Analysis = GSS.getSheetByName("분석시트");
-var sheet_Hide = GSS.getSheetByName("숨김시트");
+var sheet_Hide = GSS.getSheetByName("서버 전송 및 계산 시트");
 
 function myFunction() {
   var date = dayUpdate();
-  sheet_Hide.getRange("P3").setValue(date);
-
-  var check = true;
   var menu1Num = 0;
   var menu2Num = 0;
   var menu3Num = 0;
-  var scanPos = sheet_Hide.getRange("P5").getValue();
+  var pinCell = sheet_AutoWrite.createTextFinder(date).findAll();
 
-  if(scanPos != "#N/A"){
-    while(check){
-      var datePos = "B"+scanPos;
-      if(sheet_Hide.getRange("P4").getValue().toString().substring(8, 10) != sheet_AutoWrite.getRange(datePos).getValue().toString().substring(8, 10)){
-        check = false;
-        break;
-      }
-
-      switch(sheet_AutoWrite.getRange("D"+scanPos).getValue()){
+  for(var x = 0; x < pinCell.length; x++){
+    switch(sheet_AutoWrite.getRange(pinCell[x].getRow(), pinCell[x].getColumn()+2).getValue()){
         case "참깨라면":
           menu1Num++;
           break;
@@ -32,28 +22,27 @@ function myFunction() {
           menu3Num++;
           break;
       }
-      scanPos++;
-    }
-
-    var writePos = sheet_Hide.getRange("P6").getValue();
-
-    writeDateSell(writePos, menu1Num, menu2Num, menu3Num, sheet_Hide.getRange("P4").getValue());
   }
 
+
+  var writePosCell = sheet_Hide.createTextFinder("쓰기 위치").findAll();
+  var writePos = sheet_Hide.getRange(writePosCell[0].getRow(), writePosCell[0].getColumn() + 1).getValue();
+  writeDateSell(writePos, menu1Num, menu2Num, menu3Num, date);
 }
 
 function dayUpdate(){
   var time = new Date();
-  console.log(time);
   var month = time.getMonth()+1;
-  var day = time.getDate();
-  var returnText = time.getFullYear() + ". " +month + ". " + day;
+  var day = time.getDate() - 1;
+
+  var returnText = month + "월 " + day + "일";
 
   return returnText;
 }
 
 function writeDateSell(_writePos, _num1, _num2, _num3, _date){
-  sheet_Analysis.getRange("F"+_writePos).setValue(_date);
+  var writePosColumn = sheet_Analysis.createTextFinder("판매 분석").findAll()[0].getColumn();
+  sheet_Analysis.getRange(_writePos, writePosColumn).setValue(_date);
 
   var time = new Date();
   var day;
@@ -80,8 +69,8 @@ function writeDateSell(_writePos, _num1, _num2, _num3, _date){
       day = "금요일"
       break;
   }
-  sheet_Analysis.getRange("G"+_writePos).setValue(day);
-  sheet_Analysis.getRange("H"+_writePos).setValue(_num1);
-  sheet_Analysis.getRange("I"+_writePos).setValue(_num2);
-  sheet_Analysis.getRange("J"+_writePos).setValue(_num3);
+  sheet_Analysis.getRange(_writePos, writePosColumn + 1).setValue(day);
+  sheet_Analysis.getRange(_writePos, writePosColumn + 2).setValue(_num1);
+  sheet_Analysis.getRange(_writePos, writePosColumn + 3).setValue(_num2);
+  sheet_Analysis.getRange(_writePos, writePosColumn + 4).setValue(_num3);
 }
